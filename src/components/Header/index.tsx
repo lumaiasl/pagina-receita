51f6b4b21edc2acb24dd57b7a52591fb776da734
Shuropa <d2026006049@unifei.edu.br>
2026-07-14 19:11:24 -0300

    feat: adiciona estilo ao header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export default function Header(){
     return (
-        <header className="">
-            <h1>Receitas Deliciosas</h1>
+        <header className="w-full bg-white text-black py-4">
+            <div className="flex justify-between container mx-auto">
+                <Link className="text-xl font-bold hover:scale-105 transition-all" href="/">Receitas Deliciosas</Link>
 
-            <nav>
-                <Link href="/">Início</Link>
-                <Link href="/receitas">Receitas</Link>
-            </nav>
+                <nav className="flex gap-4">
+                    <Link className="hover:text-orange-500 transition-colors" href="/">Início</Link>
+                    <Link className="hover:text-orange-500 transition-colors" href="/receitas">Receitas</Link>
+                </nav>
+            </div>
         </header>
     )
 }
