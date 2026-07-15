d99432dddcec572709f18a1ef686833f8875375b
Shuropa <d2026006049@unifei.edu.br>
2026-07-14 23:42:02 -0300

    style: adiciona hover no botao da secao hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <div className="flex flex-col items-center gap-4 container mx-auto text-black">
             <h1 className="text-5xl">Receitas Deliciosas</h1>
             <p className="text=xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
-            <Link href="/receitas" className="bg-orange-500 text-white font-bold rounded-xl px-2 py-2">
+            <Link href="/receitas" className="bg-orange-500 text-white font-bold rounded-xl px-2 py-2 hover:bg-orange-800 transition-colors">
             Ver todas as receitas
             </Link>
           </div>
