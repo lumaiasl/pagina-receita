b1dc4be3a7d98f8f0c184551d1fd638489112da1
Shuropa <d2026006049@unifei.edu.br>
2026-07-14 23:49:39 -0300

    feat: adiciona estilizacao da secao de receitas em destaque

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
@@ -11,6 +12,17 @@ export default function Home() {
             <Link href="/receitas" className="bg-orange-500 text-white font-bold rounded-xl px-2 py-2 hover:bg-orange-800 transition-colors">
             Ver todas as receitas
             </Link>
+          </div>
+        </section>
+        <section className="py-12">
+          <div className="flex flex-col items-center">
+            <h2 className="text-lg font-bold">Receitas em Destaque</h2>
+            <div>
+
+              <Link className="flex text-orange-400 hover:text-orange-700 transition-colors" href="">
+              Ver todas as receitas<ChevronRight />
+              </Link>
+            </div>
           </div>
         </section>
     </main>
