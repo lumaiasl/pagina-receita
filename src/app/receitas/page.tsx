2d35cf186d5710ebe9d063f9b9893abcc27ec441
Shuropa <d2026006049@unifei.edu.br>
2026-07-15 00:26:13 -0300

    feat: card de receita

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -1,10 +1,15 @@
+import RecipeCard from "@/components/RecipeCards"
+import { recipes } from "@/lib/data"
+
 export default function ReceitasPage(){
     return (
-        <main className="grow py-8">
+        <main className="grow py-8 bg-white text-black">
             <div className="container mx-auto">
                 <h1 className="texst-3xl font-bold">Todas as receitas</h1>
-                <div>
-
+                <div className="mt-8 grid grid-cols-3 gap-8">
+                    {recipes.map((recipe) => 
+                        <RecipeCard />
+                    )}
                 </div>
             </div>
         </main>
