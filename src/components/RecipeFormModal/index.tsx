31d89782f80c1b7cf8bb014fcbef1301f377443c
Shuropa <d2026006049@unifei.edu.br>
2026-07-21 11:17:39 -0300

    feat: setup adicionar e remover ingredientes e instruções do form

--- src/components/RecipeFormModal/index.tsx
+++ src/components/RecipeFormModal/index.tsx
@@ -25,7 +25,7 @@ export default function RecipeFormModal({ isOpen, onClose }: RecipeFormModalProp
         onClose()
     }
     
-    const inputStyle = "p-2 border border-zinc-200 rounded-md"
+    const inputStyle = "p-2 border border-zinc-200 rounded-md grow"
     
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
@@ -68,13 +68,12 @@ export default function RecipeFormModal({ isOpen, onClose }: RecipeFormModalProp
                             {errors.imageUrl ? <span className="text-red-500 text-sm">{errors.imageUrl.message}</span> : null}
                     </div>
 
-                    <div className="grid grid-cols-3 gap-2">
+                    <div className="grid grid-cols-3 gap-1">
                         {/* Tempo de preparo */}
                         <div className="flex flex-col gap-1">
                             <label htmlFor="prepTime">Tempo de preparo</label>
                             <input className={inputStyle} type="text" id="prepTime" placeholder="30 minutos" {...register("prepTime")} />
                             {errors.prepTime ? <span className="text-red-500 text-sm">{errors.prepTime.message}</span> : null}
-
                         </div>
 
                         {/* tempo de cozimento */}
@@ -82,7 +81,6 @@ export default function RecipeFormModal({ isOpen, onClose }: RecipeFormModalProp
                             <label htmlFor="cookTime">Tempo de cozimento</label>
                             <input className={inputStyle} type="text" id="cookTime" placeholder="15 minutos" {...register("cookTime")} />
                             {errors.cookTime ? <span className="text-red-500 text-sm">{errors.cookTime.message}</span> : null}
-
                         </div>
 
                         {/* Porções */}
@@ -90,13 +88,38 @@ export default function RecipeFormModal({ isOpen, onClose }: RecipeFormModalProp
                             <label htmlFor="servings">Porções</label>
                             <input className={inputStyle} type="text" id="servings" defaultValue={1} {...register("servings")} />
                             {errors.servings ? <span className="text-red-500 text-sm">{errors.servings.message}</span> : null}
+                        </div>
+                    </div>
 
+                    {/* Ingredientes */}
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="ingredients">Ingredientes</label>
+                        <div className="flex flex-col gap-2">
+                            <div className="flex w-full gap-2">
+                                <input id="ingredients" type="text" className={inputStyle}/>
+                                <button type="button" className="bg-white border border-zinc-300 rounded-md font-semibold hover:bg-gray-200 transition-colors px-4 py-2">Remover</button>
+                            </div>
+
+                            <button type="button" className="bg-white border border-zinc-300 rounded-md font-semibold hover:bg-gray-200 transition-colors px-4 py-2 w-fit">Adicionar ingrediente</button>
+                        </div>
+                    </div>
+
+                    {/* Instruções */}
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="instructions">Instruções</label>
+                        <div>
+                            <div className="flex w-full gap-2 items-center">
+                                <textarea id="instructions" className={inputStyle}/>
+                                <button type="button" className="bg-white border border-zinc-300 rounded-md font-semibold hover:bg-gray-200 transition-colors px-4 py-2 h-fit">Remover</button>
+                            </div>
+
+                            <button type="button" className="bg-white border border-zinc-300 rounded-md font-semibold hover:bg-gray-200 transition-colors px-4 py-2 w-fit">Adicionar instrução</button> 
                         </div>
                     </div>
 
                     <div className="flex gap-2 self-end">
-                        <button type="button" onClick={onClose} className="bg-white border border-zinc-300 rounded-md hover:bg-gray-200 transition-colors px-4 py-2">Cancelar</button>
-                        <button type="submit" className="bg-black text-white rounded-md hover:bg-gray-900 transition-colors px-4 py-2">Criar Receita</button>
+                        <button type="button" onClick={onClose} className="bg-white border border-zinc-300 font-semibold rounded-md hover:bg-gray-200 transition-colors px-4 py-2">Cancelar</button>
+                        <button type="submit" className="bg-black text-white rounded-md hover:bg-gray-900 font-semibold transition-colors px-4 py-2">Criar Receita</button>
                     </div>
                 </form>
             </DialogContent>
